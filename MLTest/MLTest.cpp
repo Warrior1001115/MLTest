05e9b68549f6f50e6964b5c810d729bf03649d3c
Warrior1001115 <baimanovrustam@yandex.ru>
2023-08-18 17:23:42 +0300

    gitignore modifyed

--- MLTest/MLTest.cpp
+++ MLTest/MLTest.cpp
@@ -2,6 +2,7 @@
 #include "net.h"
 #include "trainingSet.h"
 
+// print vector label and elements
 void showVectorVals(std::string label, std::vector<double>& v) {
   std::cout << label << " ";
   for (unsigned i = 0; i < v.size(); i++) {
--- MLTest/createData.cpp
+++ MLTest/createData.cpp
@@ -1,5 +1,12 @@
 // Test data generating
 // Do not include in project.
+// 
+// 
+// To compile use Developer command prompt for VS:
+// cl createData.cpp
+// 
+// And launch to write file:
+// createData.exe > testData.txt
 
 #include<iostream>
 #include<cmath>
@@ -9,29 +16,29 @@
 
 // NAND dataset
 
-//int main() {
-//  std::cout << "topology: 2 3 1" << std::endl;
-//  for (int i = 2000; i >= 0; i--) {
-//    int first = (int)(2.0 * rand() / double(RAND_MAX));
-//    int second = (int)(2.0 * rand() / double(RAND_MAX));
-//    int result = !(first & second);
-//
-//    std::cout << "in: " << first << ".0 " << second << ".0" << std::endl;
-//    std::cout << "out: " << result << ".0" << std::endl;
-//  }
-//}
+int main() {
+  std::cout << "topology: 2 3 1" << std::endl;
+  for (int i = 2000; i >= 0; i--) {
+    int first = (int)(2.0 * rand() / double(RAND_MAX));
+    int second = (int)(2.0 * rand() / double(RAND_MAX));
+    int result = !(first & second);
 
-// exponent function dataset
-
- int main() {
-  srand(time(NULL));
-  std::cout << "topology: 1 3 1" << std::endl;
-  for (int i = 0; i < 200; i++) {
-    double x = double(i) - 100.0;
-    double y = std::exp(double(x)) + (rand() / double(RAND_MAX) - 0.5);
-
-    std::cout << std::fixed << "in: " << std::setprecision(4) << x << std::endl;
-    std::cout << std::fixed << "out: " << std::setprecision(4) << y
-              << std::endl;  
+    std::cout << "in: " << first << ".0 " << second << ".0" << std::endl;
+    std::cout << "out: " << result << ".0" << std::endl;
   }
 }
+
+// exponent function dataset (testing)
+
+// int main() {
+//  srand(time(NULL));
+//  std::cout << "topology: 1 3 1" << std::endl;
+//  for (int i = 0; i < 200; i++) {
+//    double x = double(i) - 100.0;
+//    double y = std::exp(double(x)) + (rand() / double(RAND_MAX) - 0.5);
+//
+//    std::cout << std::fixed << "in: " << std::setprecision(4) << x << std::endl;
+//    std::cout << std::fixed << "out: " << std::setprecision(4) << y
+//              << std::endl;  
+//  }
+//}
